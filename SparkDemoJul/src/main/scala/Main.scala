dcf0339ca5a3dcac7e3c349728d11bdf1ee7bfa2
Ajay054 <ajaykumar.gurram@informationtechconsultents.co.uk>
2024-08-14 15:25:58 +0100

    Dfdemo

--- SparkDemoJul/src/main/scala/Main.scala
+++ SparkDemoJul/src/main/scala/Main.scala
@@ -2,19 +2,21 @@ package org.itc.com
 
 import org.apache.commons.collections.functors.TruePredicate
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 
 object Main {
   def main(args: Array[String]): Unit = {
-   println("Hello world!")
+   //println("Hello world!")
 
-    Logger.getLogger("org").setLevel(Level.ERROR)
-
-    val sc = new SparkContext(master = "local[1]", appName = "AppName")
+   Logger.getLogger("org").setLevel(Level.ERROR)
+    val scf = new SparkConf()
+    scf.setAppName("firstdemo")
+val sc = new SparkContext(scf)
+   // val sc = new SparkContext(master = "local[1]", appName = "AppName")
  //read a file
-
-    val rdd1 = sc.textFile( path = "data.txt")
+val rdd1 =sc.textFile(args(0))
+   // val rdd1 = sc.textFile( path = "data.txt")
   //read line from the file, split by space, 1 line will give many words , 1to M
   val words = rdd1.flatMap(line => line.split(" "))
 
